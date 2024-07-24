448c0b9336c1fc295a7e40c8dc46970365e2ac14
wway <951357249@qq.com>
2024-07-24 18:17:57 +0800

    v1.3

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: wway 951357249@qq.com
  * @Date: 2023-11-02 16:40:45
  * @LastEditors: wway 951357249@qq.com
- * @LastEditTime: 2024-07-24 17:48:01
+ * @LastEditTime: 2024-07-24 18:17:46
  * @FilePath: \manage-system\vite.config.ts
  * @Description: 
  * 
@@ -13,7 +13,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: { "@": path.join(__dirname, "src/") },
